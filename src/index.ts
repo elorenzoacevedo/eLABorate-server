a179c066180ccb56883900515467c1490380e7c6
Ezequiel Lorenzo <ezequiel-lorenzo@hotmail.com>
2024-02-27 16:47:35 -0500

    add host to DataSource connection

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 
 const appDataSource = new DataSource({
   type: 'postgres',
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
