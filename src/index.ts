e3bd49da1c187e4aaf877e04372ee929eb193546
Ezequiel Lorenzo <ezequiel-lorenzo@hotmail.com>
2024-02-27 16:29:58 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+import express from 'express';
+import cors from 'cors';
+
+const appDataSource = new DataSource({
+  type: 'postgres',
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  logging: true,
+  synchronize: true,
+});
+
+const main = async () => {
+  appDataSource.initialize();
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}...`);
+  });
+};
+
+main().catch((error) => console.log(error));
