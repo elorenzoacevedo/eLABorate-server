9299157a5d4eac318b2bebb43e46653c1f0d6a5d
gfeltiii <148711859+gfeltiii@users.noreply.github.com>
2024-03-13 14:58:29 -0400

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,43 +1,54 @@
 export interface IUser {
-  pantherId: string;
-  username: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  role: string;
-  labName?: string;
-}
-
-export interface UserFilters {
-  pantherId?: string;
-  username?: string;
-  password?: string;
-  firstName?: string;
-  lastName?: string;
-  email?: string;
-  role?: string;
-  labName?: string;
-}
-
-interface DateRange {
-  start: Date;
-  end: Date;
-}
-
-export interface LabFilters {
-  name?: string;
-  startDate?: DateRange;
-  endDate?: DateRange;
-  course?: string;
-}
-
-export interface EnrollArgs {
-  pantherId: string;
-  labName: string;
-}
-
-export interface UserCredentials {
-  username: string;
-  password: string;
-}
+    pantherId: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    role: string;
+    labName?: string;
+  }
+  
+  export interface UserFilters {
+    pantherId?: string;
+    username?: string;
+    password?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    role?: string;
+    labName?: string;
+  }
+  
+  interface DateRange {
+    start: Date;
+    end: Date;
+  }
+  
+  export interface LabFilters {
+    name?: string;
+    startDate?: DateRange;
+    endDate?: DateRange;
+    course?: string;
+  }
+  
+  export interface EnrollArgs {
+    pantherId: string;
+    labName: string;
+  }
+  
+  export interface UserCredentials {
+    username: string;
+    password: string;
+  }
+  
+  export interface PostFilters {
+    datePosted?: DateRange;
+    author?: string;
+    labName?: string;
+  }
+  
+  export interface DeletionFilters {
+    moderator?: number;
+    deletionDate?: DateRange;
+  }
