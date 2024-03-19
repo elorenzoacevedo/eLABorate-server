a5c351c8c2edb7356d6233da1c6929d7b087a01f
Ezequiel Lorenzo <ezequiel-lorenzo@hotmail.com>
2024-03-19 17:43:35 -0400

    update Access-Control-Allow-Origin

--- src/index.ts
+++ src/index.ts
@@ -35,9 +35,13 @@ const main = async () => {
   app.use(cors({ origin: '*' }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
+  app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
   });
 
   app.use('/users', usersRouter);
